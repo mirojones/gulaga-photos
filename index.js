bc9a09e5e718ea1614ce491bb3ff0d9b63d0bcce
Miro <miropaxjones@gmail.com>
2024-12-16 18:03:41 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-// Photo counter and title changer...
-const titles = document.querySelectorAll('.title');
+// DOM...
+const titles = document.querySelectorAll('.title'),
+  gFigs = document.querySelectorAll('.g-fig');
+
+// Photo counter and title...
 var noOfPhotos = 100;
 
 titles.forEach(title => {
   title.textContent = noOfPhotos + ' Views of Gulaga';
+});
+
+// Image gallery...
+gFigs.forEach(gFig => {
+  var gImg = gFig.id + ' img',
+    gOpen = gFig.id + ' figure menu li .g-btn-open',
+    gDownload = gFig.id + ' figure menu li .g-btn-download';
+  gOpen.href = gImg.src;
+  gDownload.href = gImg.src;
 });
