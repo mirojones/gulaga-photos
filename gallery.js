601abb3e225a8da2bd8acfc129c9b4e8c3c67d3c
Miro <miropaxjones@gmail.com>
2024-12-16 12:25:34 +1100

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -2,4 +2,4 @@ import data from './photo-data.json' assert { type: 'json' };
 
 var photoData = JSON.parse(data);
 
-document.getElementById("gallery").innerHTML = `<p>${photoData[0].coords}</p>`;
+alert(photoData[0].coords);
