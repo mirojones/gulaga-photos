84e256c779db021c516b425c2840f692b0f262e4
Miro <miropaxjones@gmail.com>
2024-12-17 09:19:25 +1100

    Update index.js

--- index.js
+++ index.js
@@ -16,5 +16,29 @@ alert("hello");
 alert(photos[0]["coords"]);
 
 photos.forEach(photo => {
-  const gFig = document.createElment('figure');
-  const gFigCont = `<img src="${photo["url"]}"`;
+  const gFig = document.createElment('figure'),
+    gFigURL = photo['url'],
+    gFigAdr = photo['address'],
+    gFigDT = photo['datetime'],
+    gFigCont = `<img src="${gFigURL}">
+  <figcaption>
+    <button>Close</button>
+    <menu>
+      <li><a href="${gFigURL}" target="_blank">Open in full</a></li>
+      <li><a href="${gFigURL}" download>Download</a></li>
+      <li><button>About &amp; licence</button></li>
+    </menu>
+    <div>
+      <button>Close</button>
+      <table>
+        <tr>
+          <th>Location</th>
+          <td>${gFigAdr}</td>
+        </tr>
+        <tr>
+          <th>Date &amp; time</th>
+          <td>${gFigDT}</td>
+        </tr>
+      </table>
+    </div>
+  </figcaption>`;
