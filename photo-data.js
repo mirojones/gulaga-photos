521a086dec494072e6ab452c88062fa347fc76c5
Miro <miropaxjones@gmail.com>
2024-12-16 12:13:29 +1100

    Update and rename photos.json to photo-data.js

--- photo-data.js
+++ photo-data.js
@@ -1,4 +1,4 @@
-{
+photoData = '{
   "files": [
     {
       "size": "fullres",
@@ -10,4 +10,4 @@
   "datetime": "2024-09-23T05:52:00",
   "camera": "Olympus Tough TG-5",
   "settings": false
-}
+}'
