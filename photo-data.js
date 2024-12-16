4a6b9957feccbe8a2bacc063af60b628230ed468
Miro <miropaxjones@gmail.com>
2024-12-17 08:37:51 +1100

    Update photo-data.js

--- photo-data.js
+++ photo-data.js
@@ -7,7 +7,7 @@ const photos = [{
   ],
   "coords": "-36.4221452, 150.0747234",
   "address": "Sunset Point, Bermagui",
-  "datetime": "2024-09-23T05:52:00",
+  "datetime": "2024-09-23T17:52:00",
   "camera": "Olympus Tough TG-5",
   "settings": false
 }];
