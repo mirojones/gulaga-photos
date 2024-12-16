ce997ff8d5c21624c1fc66ff2ccb840c436b8238
Miro <miropaxjones@gmail.com>
2024-12-16 11:44:45 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Photo counter and title changer...
 const titles = document.querySelectorAll('.title');
+var noOfPhotos = 100;
 
 titles.forEach(title => {
   title.textContent = noOfPhotos + ' Views of Gulaga';
