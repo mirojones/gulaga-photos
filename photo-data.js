b3c23ae4a30667aa81f847482c80aba0f19d542b
Miro <miropaxjones@gmail.com>
2024-12-17 17:22:35 +1100

    Update photo-data.js

--- photo-data.js
+++ photo-data.js
@@ -7,5 +7,14 @@ var photos = [
     "datetime": "2024-09-23T17:52:00+10:00",
     "camera": "Samsung Galaxy",
     "settings": "default"
+  },
+  {
+    "id": 2,
+    "url": "images/fullres/bermagui-point_23-09-24.jpeg",
+    "coords": "-36.4221452, 150.0747234",
+    "address": "Sunset Point, Bermagui",
+    "datetime": "2024-09-23T17:52:00+10:00",
+    "camera": "Samsung Galaxy",
+    "settings": "default"
   }
 ];
