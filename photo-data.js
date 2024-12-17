5fdc0a809557c3181692f43fa8ec1679e02eac79
Miro <miropaxjones@gmail.com>
2024-12-17 13:02:07 +1100

    Update photo-data.js

--- photo-data.js
+++ photo-data.js
@@ -6,5 +6,13 @@ var photos = [
     "datetime": "2024-09-23T17:52:00",
     "camera": "Olympus Tough TG-5",
     "settings": false
+  },
+  {
+    "url": "images/fullres/bermagui-point_23-09-24.jpeg",
+    "coords": "-36.4221452, 150.0747234",
+    "address": "two",
+    "datetime": "2024-09-23T17:52:00",
+    "camera": "Olympus Tough TG-5",
+    "settings": false
   }
 ];
