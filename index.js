8f45df30b62898172aca31148f44e06843daa853
Miro <miropaxjones@gmail.com>
2024-12-17 15:39:32 +1100

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ if (urlParams.has('dt')) {
 
 photos.forEach(element => {
   const gFig = document.createElement('figure'),
+    gFigId = element["id"],
     gFigURL = element["url"],
     gFigAdr = element["address"],
     gFigDT = element["datetime"],
