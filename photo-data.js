da3313e460b0ebd9935db187f92117555ffd26e6
Miro <miropaxjones@gmail.com>
2024-12-20 13:53:15 +1100

    Update photo-data.js

--- photo-data.js
+++ photo-data.js
@@ -16,5 +16,68 @@ var photos = [
     "datetime": "2024-09-23T17:52:00+10:00",
     "camera": "Samsung Galaxy",
     "settings": "default"
+  },
+  {
+    "id": 3,
+    "url": "images/fullres/bermagui-point_23-09-24.jpeg",
+    "coords": "-36.4221452, 150.0747234",
+    "address": "Sunset Point, Bermagui",
+    "datetime": "2024-09-23T17:52:00+10:00",
+    "camera": "Samsung Galaxy",
+    "settings": "default"
+  },
+  {
+    "id": 4,
+    "url": "images/fullres/bermagui-point_23-09-24.jpeg",
+    "coords": "-36.4221452, 150.0747234",
+    "address": "Sunset Point, Bermagui",
+    "datetime": "2024-09-23T17:52:00+10:00",
+    "camera": "Samsung Galaxy",
+    "settings": "default"
+  },
+  {
+    "id": 5,
+    "url": "images/fullres/bermagui-point_23-09-24.jpeg",
+    "coords": "-36.4221452, 150.0747234",
+    "address": "Sunset Point, Bermagui",
+    "datetime": "2024-09-23T17:52:00+10:00",
+    "camera": "Samsung Galaxy",
+    "settings": "default"
+  },
+  {
+    "id": 6,
+    "url": "images/fullres/bermagui-point_23-09-24.jpeg",
+    "coords": "-36.4221452, 150.0747234",
+    "address": "Sunset Point, Bermagui",
+    "datetime": "2024-09-23T17:52:00+10:00",
+    "camera": "Samsung Galaxy",
+    "settings": "default"
+  },
+  {
+    "id": 7,
+    "url": "images/fullres/bermagui-point_23-09-24.jpeg",
+    "coords": "-36.4221452, 150.0747234",
+    "address": "Sunset Point, Bermagui",
+    "datetime": "2024-09-23T17:52:00+10:00",
+    "camera": "Samsung Galaxy",
+    "settings": "default"
+  },
+  {
+    "id": 8,
+    "url": "images/fullres/bermagui-point_23-09-24.jpeg",
+    "coords": "-36.4221452, 150.0747234",
+    "address": "Sunset Point, Bermagui",
+    "datetime": "2024-09-23T17:52:00+10:00",
+    "camera": "Samsung Galaxy",
+    "settings": "default"
+  },
+  {
+    "id": 9,
+    "url": "images/fullres/bermagui-point_23-09-24.jpeg",
+    "coords": "-36.4221452, 150.0747234",
+    "address": "Sunset Point, Bermagui",
+    "datetime": "2024-09-23T17:52:00+10:00",
+    "camera": "Samsung Galaxy",
+    "settings": "default"
   }
 ];
